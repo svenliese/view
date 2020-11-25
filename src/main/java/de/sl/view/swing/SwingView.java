91782208d12afb8ac8f830f6c3c4c1210a45ba0d
svenliese <sven_liese@web.de>
2020-11-25 08:56:05 +0100

    rectify code

--- src/main/java/de/sl/view/swing/SwingView.java
+++ src/main/java/de/sl/view/swing/SwingView.java
@@ -10,7 +10,7 @@ import java.awt.geom.Rectangle2D;
 /**
  * @author SL
  */
-public abstract class SwingView implements IView<Color> {
+public abstract class SwingView {
 
     private final IView<Color> view;
 
@@ -18,76 +18,8 @@ public abstract class SwingView implements IView<Color> {
         this.view = view;
     }
 
-    @Override
-    public float getXPercentage() {
-        return view.getXPercentage();
-    }
-
-    @Override
-    public void setXPercentage(float xPercentage) {
-        view.setXPercentage(xPercentage);
-    }
-
-    @Override
-    public float getYPercentage() {
-        return view.getYPercentage();
-    }
-
-    @Override
-    public void setYPercentage(float yPercentage) {
-        view.setYPercentage(yPercentage);
-    }
-
-    @Override
-    public float getWPercentage() {
-        return view.getWPercentage();
-    }
-
-    @Override
-    public void setWPercentage(float wPercentage) {
-        view.setWPercentage(wPercentage);
-    }
-
-    @Override
-    public float getHPercentage() {
-        return view.getHPercentage();
-    }
-
-    @Override
-    public void setHPercentage(float hPercentage) {
-        view.setHPercentage(hPercentage);
-    }
-
-    @Override
-    public boolean isInside(float xPercentage, float yPercentage) {
-        return
-            xPercentage>=view.getXPercentage() && xPercentage<=view.getXPercentage()+view.getWPercentage() &&
-            yPercentage>=view.getYPercentage() && yPercentage<=view.getYPercentage()+view.getHPercentage();
-    }
-
-    @Override
     public boolean isVisible() {
         return view.isVisible();
-    }
-
-    @Override
-    public void setVisible(boolean visible) {
-        view.setVisible(visible);
-    }
-
-    @Override
-    public boolean simulate(long now) {
-        return false;
-    }
-
-    @Override
-    public Object getUserObject() {
-        return view.getUserObject();
-    }
-
-    @Override
-    public void setUserObject(Object userObject) {
-        view.setUserObject(userObject);
     }
 
     protected static String trim(String origin, Graphics2D g2d, double width) {
